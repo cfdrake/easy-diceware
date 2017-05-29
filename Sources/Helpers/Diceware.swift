f01b302913d9d0f76ffd9b44f37425e9086c0fef
Colin Drake <colin.f.drake@gmail.com>
2017-05-28 20:37:49 -0500

    Add in Diceware implementation

--- Sources/Helpers/Diceware.swift
+++ Sources/Helpers/Diceware.swift
@@ -8,10 +8,44 @@
 
 import Foundation
 
-func throwDie() -> Int {
-    return Int(arc4random() % 6) + 1
-}
+/// Represents the whole diceware system.
+final class Diceware {
 
-func throwNDice(n: Int) -> [Int] {
-    return (0..<n).map { _ in return throwDie() }
+    // MARK: Properties
+
+    var dicewareEntries: [String: String] = [:]  // Hash table for quick searching.
+
+    // MARK: Helpers
+
+    func throwDie() -> Int {
+        return Int(arc4random() % 6) + 1
+    }
+
+    func throwNDice(n: Int) -> [Int] {
+        return (0..<n).map { _ in return throwDie() }
+    }
+
+    func buildDictionary() {
+        let dicewarePath = Bundle.main.path(forResource: "diceware", ofType: "txt")!
+        guard let dicewareLines = try? String(contentsOfFile: dicewarePath).components(separatedBy: "\n") else {
+            fatalError("Could not load diceware entries")
+        }
+
+        dicewareLines.filter { $0.characters.count > 0 }.forEach { (line: String) in
+            let components = line.components(separatedBy: "\t")
+            self.dicewareEntries[components[0]] = components[1]
+        }
+    }
+
+    // MARK: Public Interface
+
+    func randomPassphraseOfLength(_ length: Int) -> String {
+        if dicewareEntries.count == 0 {
+            buildDictionary()
+        }
+
+        let numbers = (0..<length).map { _ in return throwNDice(n: 5).map({ String($0) }).joined() }
+        return numbers.map({ num in dicewareEntries[num]! }).joined(separator: " ")
+    }
+
 }
